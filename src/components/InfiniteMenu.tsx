28e9de883251efcc6cbe888f42920d278ec4536d
Suyash Singh <suyashsingh667@gmail.com>
2026-07-25 01:09:57 +0530

    fix: Re-enable UNPACK_FLIP_Y_WEBGL to correct texture coordinates and map clicks correctly

--- src/components/InfiniteMenu.tsx
+++ src/components/InfiniteMenu.tsx
@@ -924,6 +924,7 @@ class InfiniteGridMenu {
       });
 
       gl.bindTexture(gl.TEXTURE_2D, this.tex);
+      gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
       gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, canvas);
       gl.generateMipmap(gl.TEXTURE_2D);
     });
