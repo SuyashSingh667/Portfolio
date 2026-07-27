b41d85203bf2b053cb9bb8297dfc1c2353529cb1
Suyash Singh <suyashsingh667@gmail.com>
2026-07-28 01:23:48 +0530

    style: Decrease center project scale to 0.18 and set camera distance 3.4 so surrounding corner projects are more visible on main screen

--- src/components/InfiniteMenu.tsx
+++ src/components/InfiniteMenu.tsx
@@ -675,7 +675,7 @@ class InfiniteGridMenu {
     this.onActiveItemChange = onActiveItemChange || (() => {});
     this.onMovementChange = onMovementChange || (() => {});
     this.scaleFactor = scale;
-    this.camera.position[2] = 3 * scale;
+    this.camera.position[2] = 3.4 * scale;
     this.#init(onInit);
   }
 
@@ -849,8 +849,8 @@ class InfiniteGridMenu {
     this.control.update(deltaTime, this.TARGET_FRAME_DURATION);
 
     let positions = this.instancePositions.map(p => vec3.transformQuat(vec3.create(), p, this.control.orientation));
-    const scale = 0.25;
-    const SCALE_INTENSITY = 0.6;
+    const scale = 0.18;
+    const SCALE_INTENSITY = 0.5;
     positions.forEach((p, ndx) => {
       const s = (Math.abs(p[2]) / this.SPHERE_RADIUS) * SCALE_INTENSITY + (1 - SCALE_INTENSITY);
       const finalScale = s * scale;
