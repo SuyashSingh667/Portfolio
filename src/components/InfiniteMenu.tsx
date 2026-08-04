3906ac541ebf58d5e019fdfe4a220e021b423ce5
Suyash Singh <suyashsingh667@gmail.com>
2026-08-04 19:57:09 +0530

    Make project slightly bigger

--- src/components/InfiniteMenu.tsx
+++ src/components/InfiniteMenu.tsx
@@ -965,7 +965,8 @@ class InfiniteGridMenu {
     const canvasEl = gl.canvas as HTMLCanvasElement;
     this.camera.aspect = canvasEl.clientWidth / canvasEl.clientHeight;
     // Base height factor that works perfectly at 100% zoom on a standard screen
-    const baseHeight = this.SPHERE_RADIUS * 0.35;
+    // Decreased slightly from 0.35 to 0.32 to make the elements a very little bit bigger
+    const baseHeight = this.SPHERE_RADIUS * 0.32;
     
     // Calculate zoom compensation so that the WebGL objects shrink when zooming out
     // just like normal CSS elements would, instead of remaining locked to 100vh.
